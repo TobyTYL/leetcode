1f047ab03959f91124b50be11b75378fb416d5ed
Toby Tao <yilintao@TobydeMacBook-Pro.local>
2023-03-23 14:50:01 -0400

    ss

---
+++ day27.md
@@ -0,0 +1,60 @@
+# Day 27
+
+- [x] LC 39, 40, 131
+
+# LC 39. Combination Sum
+
+Description: 
+Given an array of distinct integers candidates and a target integer target, return a list of all unique combinations of candidates where the chosen numbers sum to target. You may return the combinations in any order.
+
+The same number may be chosen from candidates an unlimited number of times. Two combinations are unique if the  frequency of at least one of the chosen numbers is different.
+
+The test cases are generated such that the number of unique combinations that sum up to target is less than 150 combinations for the given input.
+
+
+Example:
+```
+Input: candidates = [2,3,6,7], target = 7
+Output: [[2,2,3],[7]]
+Explanation:
+2 and 3 are candidates, and 2 + 2 + 3 = 7. Note that 2 can be used multiple times.
+7 is a candidate, and 7 = 7.
+These are the only two combinations.
+```
+
+
+Ans:
+
+```py
+class Solution(object):
+    def combinationSum(self, candidates, target):
+        """
+        :type candidates: List[int]
+        :type target: int
+        :rtype: List[List[int]]
+        """
+        path = []
+        result = []
+        def back_tracking(self, candidates, target, sum_v, start_index):
+            if sum_v == target:
+                # 因为是shallow copy，所以不能直接传入self.path
+                result.append(path[:])
+                return
+                
+            # 因为本题没有组合数量限制，所以只要元素总和大于target就算结束
+            if sum_v > target:
+                return
+
+            # 单层递归逻辑 
+            for i in range(start_index, len(candidates)):
+                sum_v += candidates[i]
+                path.append(candidates[i])
+                # 注意递归逻辑，i递归之后还是加i
+                back_tracking(self, candidates, target, sum_v, i)
+                # 回溯
+                sum_v -= candidates[i]
+                path.pop()
+        back_tracking(self, candidates, target, 0, 0)
+        return result
+```
+
--- test.py
+++ test.py
@@ -1,57 +1,6 @@
-# The embedded software that controls a military satellite suffers from software aging. This satellite’s availability must be no less than six nines (99.9999 %), which means a yearly downtime no longer than 31.56 seconds. Because of the accumulation of the aging effects, the size of the satellite’s main application process grows progressively. The memory size of this process has been monitored monthly, for one year, and the result follows ( in megabyte): 50, 293, 763, 1097, 1355, 1567, 1745, 1900, 2037, 2159, 2269, 2370. It is known that once this process grows beyond 2.8 gigabytes, it will be abruptly forced to terminate by the satellite operating system. Taking into consideration the above-described scenario, answer the following questions:
+# We will call a sequence of integers a spike if they first increase (strictly) and then decrease (also strictly, including the last element of t increasing part).
+# For example(4, 5, 7, 6, 3, 2) is a spike, but(1, 1, 5, 4, 3) and (1, 4, 3, 5) are not.
+#  Note that the increasing and decreasing parts always intersect, e.g.: for spike(3, 5, 2) sequence(3, 5) is an increasing part and sequence(5, 2) is a decreasing part, and for spik (2) sequence(2) is both an increasing and a decreasing part.
+# Your are given an array A of N integers. Your task is to calculate the length of the longest possible spike, which can be created from numbers from array A. Note that you are NOT supposed to find the longest spike as a sub-sequence of A, but rather choose some numbers from A and reorder them to create the longest spike.
 
-# To create the python code
-
-
-import numpy as np
-import matplotlib.pyplot as plt
-from scipy.optimize import curve_fit
-
-# To create the data
-x = np.array([1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12])
-y = np.array([50, 293, 763, 1097, 1355, 1567,
-             1745, 1900, 2037, 2159, 2269, 2370])
-
-# To create the function
-
-
-def func(x, a, b, c):
-    return a * np.exp(b * x) + c
-
-
-# To create the curve fit
-popt, pcov = curve_fit(func, x, y)
-
-# To create the plot
-plt.plot(x, y, 'ko', label="Original Noised Data")
-plt.plot(x, func(x, *popt), 'r-', label="Fitted Curve")
-plt.legend()
-plt.show()
-
-# To print the result
-print(popt)
-
-# To print the result
-print(pcov)
-
-# To print the result
-print(func(13, *popt))
-
-# To print the result
-print(func(13, *popt) - 2800)
-
-# To print the result
-
-print(func(13, *popt) - 2800 > 0)
-
-
-# To print the result
-print(func(13, *popt) - 2800 < 0)
-
-# To print the result
-print(func(13, *popt) - 2800 == 0)
-
-# To print the result
-print(func(13, *popt) - 2800 != 0)
-
-# To print the result
+def solution(lst):
